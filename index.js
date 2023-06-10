9a2528b56963e6962511f45ed0796044cd36e27d
Mykola Voinikov <qwoxa1@gmail.com>
2023-06-10 12:41:34 +0300

    fix: add dots

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import includes from "lodash/includes";
 
 export default function checkAppliactionStatus(input) {
   if (includes(input, "error")) {
-    return "Application failed";
+    return "Application failed.";
   }
 
-  return "Application running";
+  return "Application running.";
 }
